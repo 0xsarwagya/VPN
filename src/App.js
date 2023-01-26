8f503fdac2cad9afaa422d7a63250a2b096e9a12
0xsarwagya <sarwagyasingh69@gmail.com>
2023-01-26 17:39:27 +0100

    Added Base File

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [status, setStatus] = useState("Disconnected");
+
+  const connectToVPN = async () => {
+    console.log('Connection Started')
+    try {
+      // Code to initiate a VPN connection using the WebRTC API
+      const pc = new RTCPeerConnection();
+      const dc = pc.createDataChannel("vpn");
+      dc.onopen = () => {
+        setStatus("Connected");
+        console.log("VPN connection established");
+      };
+      dc.onclose = () => {
+        setStatus("Disconnected");
+        console.log("VPN connection closed");
+      };
+      await pc.setLocalDescription(await pc.createOffer());
+      // Code to send the local description to the VPN server
+    } catch (error) {
+      setStatus("Disconnected");
+      console.error("Error establishing VPN connection:", error);
+    }
+  };
+
+  const disconnectFromVPN = () => {
+    // Code to terminate the VPN connection
+    setStatus("Disconnected");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>VPN Client</h1>
+      <p>Status: {status}</p>
+      {status === "Disconnected" && (
+        <button className="connect-button" onClick={connectToVPN}>
+          Connect to VPN
+        </button>
+      )}
+      {status === "Connected" && (
+        <button className="disconnect-button" onClick={disconnectFromVPN}>
+          Disconnect from VPN
+        </button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
